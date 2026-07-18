8ff9b448c63919d48d9206cbb10b3a71b5cfba3e
pawankool2k-droid <pawankool2k@gmail.com>
2026-07-17 22:39:17 -0400

    Enable middleware mode for server and preview

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  server: {
+    middlewareMode: true,
+  },
+  preview: {
+    middlewareMode: true,
+  },
 })
